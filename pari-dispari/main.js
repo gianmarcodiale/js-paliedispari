dc86cb312eda9ff4bcaceb6aef7e09c1cba7cb77
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-04-01 18:01:07 +0200

    correzione pari e dispari

--- pari-dispari/main.js
+++ pari-dispari/main.js
@@ -7,26 +7,38 @@ Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
 
+let userEvenOrOdd = prompt("Scegli pari o dispari");
 let userNumber = parseInt(prompt('Scegli un numero da 1 a 5.'));
 let pcNumber = pcRandNumber(1, 5);
 let numberSum = userNumber + pcNumber;
-console.log("L'UTENTE HA IL DISPARI");
-console.log("IL COMPUTER HA IL PARI");
 
-function pcRandNumber (min, max) {
+/* if (userEvenOrOdd == "pari") {
+    console.log("L'utente ha scelto pari, il computer sarà dispari.");
+} else {
+    console.log("L'utente ha scelto dispari, il computer sarà pari");
+}; */
+
+function pcRandNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-function evenOrOdd (number) {
-    if (number % 2 != 0) {
+function evenOrOdd(number) {
+    if (number % 2 === 0) {
         return true;
     } else {
         return false;
     };
 };
 
-if (evenOrOdd(numberSum)) {
-    console.log(`L'utente ha vinto. Il computer ha scelto il numero ${pcNumber}. La somma è ${numberSum}`);
+if (evenOrOdd(numberSum) == true && userEvenOrOdd === "pari") {
+    console.log(`L'utente ha vinto! La somma dei numeri è ${numberSum}. Il computer ha scelto il numero ${pcNumber}`);
+} else if (evenOrOdd(numberSum) == false && userEvenOrOdd === "pari") {
+    console.log(`Il computer ha vinto! La somma dei numeri è ${numberSum}. Il computer ha scelto il numero ${pcNumber}`);
+} else if (evenOrOdd(numberSum) == false && userEvenOrOdd === "dispari") {
+    console.log(`L'utente ha vinto! La somma dei numeri è ${numberSum}. Il computer ha scelto il numero ${pcNumber}`);
 } else {
-    console.log(`Il computer ha vinto. Il computer ha scelto il numero ${pcNumber}. La somma è ${numberSum}`);
-};
+    console.log(`Il computer ha vinto! La somma dei numeri è ${numberSum}. Il computer ha scelto il numero ${pcNumber}`);
+}
+
+
+
